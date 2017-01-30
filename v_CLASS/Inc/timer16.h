fb2ce4f6f1b78ce31c28f2c28c05bb013f7afbcc
Alexey <p@52.ru>
2017-01-30 12:20:34 +0400

    cleared

--- v_CLASS/Inc/timer16.h
+++
@@ -1,9 +0,0 @@
-#ifndef _TIMER16_H
-#define _TIMER16_H
-
-void TIM1_UP_TIM16_IRQHandler(void);
-void TIM16_Init(void);
-void TIM16_Start(void);
-void TIM16_Stop(void);
-void TIM16_Reset(void);
-#endif
--- v_CLASS/Inc/timer6.h
+++
@@ -1,6 +0,0 @@
-#ifndef _TIMER6_H
-#define _TIMER6_H
-
- void TIM13_IRQHandler(void);
-
-#endif
